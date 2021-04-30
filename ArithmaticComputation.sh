e070f65e2d9ab669f523724e63e9f4ff23642685
Ranjithgc <ranjith.amca.16@acharya.ac.in>
2021-04-30 11:03:46 +0530

    Add: Arithmatic Computation

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -1,2 +1,11 @@
 #!/bin/bash -x 
 echo "Welcome to the Arithmatic Computation And Sorting"
+echo "Enter the first Number"
+read a
+echo "Enter the second Number"
+read b
+echo "Enter the third Number"
+read c
+echo "First Number:$a"
+echo "Second Number:$b"
+echo "Third Number:$c"
