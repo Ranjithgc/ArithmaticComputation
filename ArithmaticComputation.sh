c4b654ec08daa5a1b2f75ce9adb947856409c099
Ranjithgc <ranjith.amca.16@acharya.ac.in>
2021-04-30 13:00:10 +0530

    Add: Arithmatic Computation

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -9,10 +9,14 @@ read c
 echo "First Number:$a"
 echo "Second Number:$b"
 echo "Third Number:$c"
-echo $(($a+$b*$c))
-echo $(($a*$b+$c))
-echo $(($c+$a/$b))
-echo $(($a%$b+$c))
+result1= $(($a+$b*$c))
+result2= $(($a*$b+$c))
+result3= $(($c+$a/$b))
+result4= $(($a%$b+$c))
+echo $result1
+echo $result2
+echo $result3
+echo $result4
 declare -A test_var
 test_var[key1]=$result1
 test_var['key2']=$result2
@@ -25,3 +29,14 @@ echo ${test_var[key1]}
 echo ${test_var[key2]}
 echo ${test_var[key3]}
 echo ${test_var[$another_key_var]}
+counter=0
+Array[((counter++))]=$result1
+Array[((counter++))]=$result2
+Array[((counter++))]=$result3
+Array[((counter++))]=$result4
+echo ${Array[@]}
+echo "Original Numbers in array:"
+for (( i = 0; i <= 3; i++ ))
+do
+      echo ${Array[$i]}
+done
